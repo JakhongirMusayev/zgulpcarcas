251b359b33d5c5893bbbc680215addbaea5562de
JakhongirMusayev <DevDar76uzb@gmail.com>
2020-12-19 04:09:26 +0500

    pug2html va html sozlandi

--- gulpfile.js
+++ gulpfile.js
@@ -17,27 +17,31 @@ let path = {
 	},
 	// Object s putyami k razrabatiyvaem papkam
 	src: {
-		html: sourceFolder + "/",
-		css: sourceFolder + "/scss/main.scss",
+		// vaqtincha 'pug' o'rniga 'html' va 'gulp-file-include'dan foydalanib ko'rishga qaror qildim (2-commit jarayonida).
+		// "!" + sourceFolder + "/*.html" - isklyuchayem fayliy nachinayushiesya s nijnim podcherkivaniyem. Takje ne zabiyvayem vnesti izmeneniya '@@include' v index.html fayle!
+		html: [sourceFolder + "/*.html", "!" + sourceFolder + "/_*.html"],
+		scss: sourceFolder + "/scss/main.scss",
 		js: sourceFolder + "/js/script.js",
 		fonts: sourceFolder + "/fonts/**/*.{ttf,woff, woff2}",
-		images: sourceFolder + "/images/**/*.{jpg,pug,svg,gif,ico,webp}",
+		images: sourceFolder + "/images/**/*.{jpg,html,svg,gif,ico,webp}",
 		// V 'fonts' i 'images' ukaziyvayem te rasshireniya kotoriyye my xotim ispolzovat, ne ukazanniyye rasshireniya budut ignorirovatsya.
 	},
 	// Object s putyami k papkam i failam, kotoriyx my doljni otlavlivat i slushat (watch) postoyanno
 	watch: {
-		html: sourceFolder + "/**/*.pug",
+		html: sourceFolder + "/**/*.html",
 		css: sourceFolder + "/scss/**/*.scss",
 		js: sourceFolder + "/js/**/*.js",
 		// fonts: sourceFolder + "/fonts/**/*.{ttf,woff, woff2}", // Shrifty postoyanno slushat ne obyazatelno
-		images: sourceFolder + "/images/**/*.{jpg,pug,svg,gif,ico,webp}",
+		images: sourceFolder + "/images/**/*.{jpg,html,svg,gif,ico,webp}",
 	},
 	// Object soderjashiy put k papke proyekta. Object, kotoriy udalyayet etu papku kajdiy raz dlya obnovleniya
 	clean: "./" + projectFolder + "/"
 }
 // const { src, dest } = require('gulp');
+const { src, dest } = require('gulp');
 const gulp = require('gulp');
 const browsersync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
 
 
 
@@ -51,8 +55,27 @@ function browserSync(params) {
 		notify: false // otklyuchyet vspliyvayushee okno ob obnovlenii stranitsy (chtobiy ne otvlekalo)
 	})
 }
+
+function html() {
+	// vozvrashat 'html' - kotoriy v - 'src', kotoriy ukazan v peremennoy 'path'
+	return src(path.src.html)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream())
+}
+// Slejka za izmeneniyem faylov (Ne zabiyvayam dobavlyat v 'watch').
+function watchFiles() {
+	gulp.watch([path.watch.html], html);
+}
+
+
+
+let build = gulp.series(html);
 // Kogda 'watch' - viypolnyayetsya 'browser-sync'. Shu yerda xato qilganman: 'browsersync' deganman, vaholanki u 'browserSync' nomli function ichida ishlatilgani uchun uning o'ziga emas, balki functionga murojaat qilishim kerak ekan!
-let watch = gulp.parallel(browserSync);
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.html = html;
+exports.build = build;
 // 'exports' - chtoby 'gulp' znal, ponimal i rabotal s peremenniymi
 exports.watch = watch;
 // Kogda zapuskayem proyekt po umolchaniyu budet viypolnyatsya 'watch', kotory v svoyu ochered budet zapuskat 'browser-sync'
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 1. "Gulp"ning o'zi sozlandi, keyingi bosqich "pug" va boshqalarni sozlash.
-
-# Izoh: "pug" va boshqalar sozlanmaganligi uchun hozircha vrladkada 'error' va stranitsada 'Cennot Get /' ko'rsatilmoqda.
+   1.1. Izoh: "pug" va boshqalar sozlanmaganligi uchun hozircha vrladkada 'error' va stranitsada 'Cennot Get /' ko'rsatilmoqda.
+2. 2-commit jarayonida 'let watch = gulp.parallel(build, browserSync);' 'build'ni qo'shish esimdan chiqqani uchun rosa vaqtim ketdi (Men 'pug'ni 'html'ga o'zgartirganimda xatolik yuz berdimi deb o'ylagandim, unday emas ekan).
+   2.1. Izoh: 'pug' 'html'ga o'zgartirildi va 'html'ni bo'laklarga (\_header.html tashkil etildi) bo'lib ishlatish uchun 'gulp-file-include' o'rnatilib sozlandi.
+   2.2. 'gulp-file-include'ning o'zida 'html'bibg bo'laklarini inobatga olmaslik yo'qligi uchun ('pug'ning o'zida bor) 'header.html' ''\_header.html'ga o'zgartirilib, 'gulpfile.js'da inkor etish operatoridan foydalanib 'index.html'dan boshqalarini kuzatmaslik (watch) joriy etildi.
