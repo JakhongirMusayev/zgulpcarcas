ad81b4b809c226502820f261777b8b9dcc8153b1
JakhongirMusayev <DevDar76uzb@gmail.com>
2020-12-19 05:16:02 +0500

    css va unga bog'liqlar sozlandi

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ let path = {
 		// vaqtincha 'pug' o'rniga 'html' va 'gulp-file-include'dan foydalanib ko'rishga qaror qildim (2-commit jarayonida).
 		// "!" + sourceFolder + "/*.html" - isklyuchayem fayliy nachinayushiesya s nijnim podcherkivaniyem. Takje ne zabiyvayem vnesti izmeneniya '@@include' v index.html fayle!
 		html: [sourceFolder + "/*.html", "!" + sourceFolder + "/_*.html"],
-		scss: sourceFolder + "/scss/main.scss",
+		css: sourceFolder + "/scss/main.scss",
 		js: sourceFolder + "/js/script.js",
 		fonts: sourceFolder + "/fonts/**/*.{ttf,woff, woff2}",
 		images: sourceFolder + "/images/**/*.{jpg,html,svg,gif,ico,webp}",
@@ -42,6 +42,15 @@ const { src, dest } = require('gulp');
 const gulp = require('gulp');
 const browsersync = require('browser-sync').create();
 const fileinclude = require('gulp-file-include');
+const del = require('del');
+const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+// 'gulp-group-css-media-queries' - media-zaproslarni bitta joyga yig'ib, eng tagiga qo;yadi
+const groupmediaqueries = require('gulp-group-css-media-queries');
+// css fayllarni tozalaydi va siqadi (sjimayet)
+const cleancss = require('gulp-clean-css');
+// ikkita css file tashkil qilish uchun: biri oddiy, ikkinchisi siqilgan (sjatiy-.min.css)
+const rename = require('gulp-rename');
 
 
 
@@ -61,19 +70,49 @@ function html() {
 	return src(path.src.html)
 		.pipe(fileinclude())
 		.pipe(dest(path.build.html))
-		.pipe(browsersync.stream())
+		.pipe(browsersync.stream());
+}
+function css() {
+	return src(path.src.css)
+		.pipe(
+			scss({
+				outputStyle: 'expanded'
+			})
+		)
+		.pipe(groupmediaqueries())
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ['last 5 versions'],
+				cascade: true
+			})
+		)
+		.pipe(dest(path.build.css)) // Oldin birinchi: oddiy css fayl chiqaramiz (1)
+		.pipe(cleancss()) // keyin tozalaymiz (2)
+		.pipe(
+			rename({
+				extname: '.min.css'
+			})
+		) // boshqacha nomlaymiz (3)
+		.pipe(dest(path.build.css)) // va ikkinchi: siqilgan hamda boshqa nomlangan css fayl chiqaramiz (4)
+		.pipe(browsersync.stream());
 }
 // Slejka za izmeneniyem faylov (Ne zabiyvayam dobavlyat v 'watch').
 function watchFiles() {
 	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+}
+// Ochistka (udaleniye) papki 'dist'
+function clean() {
+	return del(path.clean);
 }
 
 
 
-let build = gulp.series(html);
+let build = gulp.series(clean, gulp.parallel(html, css));
 // Kogda 'watch' - viypolnyayetsya 'browser-sync'. Shu yerda xato qilganman: 'browsersync' deganman, vaholanki u 'browserSync' nomli function ichida ishlatilgani uchun uning o'ziga emas, balki functionga murojaat qilishim kerak ekan!
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 // 'exports' - chtoby 'gulp' znal, ponimal i rabotal s peremenniymi
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 2. 2-commit jarayonida 'let watch = gulp.parallel(build, browserSync);' 'build'ni qo'shish esimdan chiqqani uchun rosa vaqtim ketdi (Men 'pug'ni 'html'ga o'zgartirganimda xatolik yuz berdimi deb o'ylagandim, unday emas ekan).
    2.1. Izoh: 'pug' 'html'ga o'zgartirildi va 'html'ni bo'laklarga (\_header.html tashkil etildi) bo'lib ishlatish uchun 'gulp-file-include' o'rnatilib sozlandi.
    2.2. 'gulp-file-include'ning o'zida 'html'bibg bo'laklarini inobatga olmaslik yo'qligi uchun ('pug'ning o'zida bor) 'header.html' ''\_header.html'ga o'zgartirilib, 'gulpfile.js'da inkor etish operatoridan foydalanib 'index.html'dan boshqalarini kuzatmaslik (watch) joriy etildi.
+3. Quyidagilar sozlandi:
+   "del": "^6.0.0",
+   "gulp-autoprefixer": "^7.0.1",
+   "gulp-clean-css": "^4.3.0",
+   "gulp-group-css-media-queries": "^1.2.2",
+   "gulp-rename": "^2.0.0",
+   "gulp-sass": "^4.1.0"
